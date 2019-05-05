6b6b8feaf0b62dca35cde3f9c5e19629073d797e
Anne Edmondson <edmondson.anne@gmail.com>
2019-05-05 02:23:44 +0000

    Finalizing CLI. Not working yet.

--- lib/rockies/cli.rb
+++ lib/rockies/cli.rb
@@ -1,13 +1,15 @@
 class Rockies::CLI
   def start
-    puts "Hello Rockies fan! Here is the active pitching roster for the Colorado Rockies 2019-2020 season. "
     Rockies::Scraper.new.scrape_roster
+    puts "Hello Rockies fan! Here is the active pitching roster for the Colorado Rockies 2019-2020 season. "
     list_roster
     menu
+    program_exit
   end
 
   def list_roster
-    Rockies::Ballplayer.all.each.with_index(1) do |ballplayer, i|
+    @ballplayers = Rockies::Ballplayer
+    @ballplayers.all.each.with_index(1) do |ballplayer, i|
       puts "#{i}. #{ballplayer.name.strip}"
     end
   end
@@ -19,18 +21,19 @@ class Rockies::CLI
       puts "If you'd like to see the list of pitchers again, enter 'list'."
       puts "Enter 'exit' to leave the program."
       input = gets.strip.downcase
-      case input
-        when "1"
-          puts "More about this pitcher"
-        when "2"
-          puts "And more about this pitcher"
-        when "list"
-          list_roster
-        when "exit"
-          puts "Enjoy the baseball season! Go Rockies!"
-        else
-          puts "Whoops. That's an invalid entry. Please try again."
-       end
+
+      if input.to_i > 0
+        ballplayer = @ballplayers[input.to_i-1]
+        puts "#{ballplayer.name} wears jersey number #{ballplayer.jersey_no} and bats/throws #{ballplayer.bat_throw_position}. He is #{ballplayer.height} tall and weighs #{ballplayer.weight}. He was born on #{ballplayer.dob}."
+      elsif input = "list"
+        list_roster
+      else
+        puts "Whoops. That's an invalid entry. Please try again."
+      end
      end
+  end
+
+  def program_exit
+    puts "Enjoy the baseball season! Go Rockies!"
   end
 end
